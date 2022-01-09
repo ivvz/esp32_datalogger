8b9658d93f12c169dadc276dc47e675151996109
ivvz <ivanwzh.iqm@gmail.com>
2022-01-09 02:21:48 -0600

    agregue una pantalla y muestra la temperatura de 3 termopares aunque solo uno este conectado gg

--- datalogger/datalogger.ino
+++ datalogger/datalogger.ino
@@ -1,20 +1,45 @@
 #include "FS.h"
 #include "SD.h"
-#include "SPI.h"
 #include "max6675.h"
+#include <RTClib.h> 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+
+
+
+unsigned long tiempo_lectura = 2000; // intervalo de lectura general (sin ejecutar funcion de registro de datos)
+unsigned long tiempo_sin_registro;
 
 int thermoDO = 17;
 int thermoCS = 16;
 int thermoCLK = 4;
 
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+MAX6675 termopar1(thermoCLK, thermoCS, thermoDO);
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 
 void setup(){
+  Wire.begin(33, 32);
   Serial.begin(115200);
-
+  
+  checkOLED();
+  display.display();
+  delay(2000); // Pause for 2 seconds
+  
   checkSD();
+  
+
 
   
   writeFile(SD, "/hello00.txt", "Hello ");
@@ -27,27 +52,125 @@ void setup(){
 }
 
 void loop(){
-   Serial.print("C = "); 
-   Serial.println(thermocouple.readCelsius()); 
-   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
-   delay(1000);
+  
+  if(millis() - tiempo_sin_registro >= tiempo_lectura){
+     //hora = getTime();
+     Serial.println(" Termopar 1 = " + String(termopar1.readCelsius()) + " C");
+     //Serial.println(hora + " Termopar 2 = " + String(termopar2.readCelsius()) + " C");
+     display.clearDisplay();
+     display.setTextSize(1);             
+     display.setTextColor(WHITE);        
+     display.setCursor(0,10);             
+     display.print("Tp 1");
+     display.setTextSize(2);  
+     display.setCursor(30,10);             
+     display.print(String(termopar1.readCelsius()) + " C");
 
+     display.setTextSize(1);
+     display.setCursor(0,30);             
+     display.print("Tp 2");
+     display.setTextSize(2);  
+     display.setCursor(30,30);             
+     display.print(String(termopar1.readCelsius()+10) + " C");
+     display.display();
+
+     display.setTextSize(1);
+     display.setCursor(0,50);             
+     display.print("Tp 3");
+     display.setTextSize(2);  
+     display.setCursor(30,50);             
+     display.print(String(termopar1.readCelsius()+16) + " C");
+     display.display();
+     
+     
+     tiempo_sin_registro = millis();  
+    }
+}
+
+
+
+String createString(){
+  // crea el string para csv con las temperaturas
+    String dataString = ""; 
+    float lectura1 = termopar1.readCelsius();
+    float lectura2 = termopar1.readCelsius() + 21;
+    float lectura3 = termopar1.readCelsius() + 14;
+    dataString += String(millis()) + ",";
+    dataString += String(lectura1);
+    dataString += ","; 
+    dataString  += String(lectura2);
+    dataString += ","; 
+    dataString  += String(lectura3);
+
+  return dataString;
+}
+
+ File createFile(String nombre){
+  writeFile(SD, "/hello00.txt", "Hello ");
+  //File dataFile = SD.open(nombre + ".csv", FILE_WRITE);
+  String header = ("hora,termopar 1,termopar 2,termopar 3");
+  if (dataFile) {
+    dataFile.println(header);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(header);
+    delay(5000);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error creating datafile");
+  }
+  return dataFile;
 }
 
 
 void checkSD(){
+    display.setTextSize(2);             
+    display.setTextColor(WHITE);        
+    display.setCursor(2,20);             
+    display.println("REVISANDO MICRO SD!");
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+    
     if(!SD.begin(5)){
     Serial.println("Card Mount Failed");
+    display.setTextSize(2);             
+    display.setTextColor(WHITE);        
+    display.setCursor(10,20);             
+    display.println(F("FALL0 SD!"));
+    display.display();
+    delay(2000);
+    display.clearDisplay();
     return;
+  }else{  
+    display.setTextSize(3);             
+    display.setTextColor(WHITE);        
+    display.setCursor(40,10);             
+    display.println("SD");
+    display.setCursor(40,40);
+    display.println("OK!");
+    display.display();
+    delay(2000);
+    display.clearDisplay();
   }
   
   uint8_t cardType = SD.cardType();
 
   if(cardType == CARD_NONE){
-    Serial.println("No SD card attached");
+    Serial.println("No SE DETECTO UNA TARJETA");
     return;
   }
   }
+
+void checkOLED(){
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+}
+
+
 
 
 
